bfaa7c3a08833c7dd80fa6bf5ce98307e87c0230
ashaysri <srivastavaashay2013@gmail.com>
2024-04-12 22:00:54 +0530

    Setup Tailwind and basic Layout

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+    let tabs = [
+        {name: 'Projects', href: '#projects'},
+        {name: 'About Me', href: '#about'}
+    ]
+</script>
+
+<header class="sticky z-[10] duration-200 top-0 px-6 flex justify-between bg-slate-950/75">
+    <h1 class="m-4 p-2 font-medium">
+        <b>Ashay</b> Srivastava
+    </h1>
+    <div class="m-4">
+        {#each tabs as tab,index}
+            <a class="duration-400 hover:text-violet-400 m-2" href={tab.href}>{tab.name}</a>
+        {/each}
+        <button class="p-2 px-3 bg-white text-slate-950 rounded-full hover:bg-slate-200">Let's Connect</button>
+    </div>
+</header>
+
---
+++ src/components/Main.svelte
@@ -0,0 +1,4 @@
+
+<p class="px-6 text-4xl">
+    Hi I'm Ashay Srivastava, a full stack developer
+</p>
